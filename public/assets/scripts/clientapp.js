9644812dd8b46f9d0086f4e861124f6a9d124deb
Bennett Batzli <bennett.batzli@gmail.com>
2016-09-08 23:12:11 -0500

    deleted commented-out code

--- public/assets/scripts/clientapp.js
+++ public/assets/scripts/clientapp.js
@@ -11,37 +11,5 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: '../../views/templates/discoverMusic.html',
       controller: 'discoverMusicController'
     });
-      //templateUrl: '../../views/templates/home.html',
-      //controller: 'homeController',
-    //  views: {
-    //    'header': {
-    //      templateUrl: '../../views/templates/header.html'
-    //    },
-    //    'nav': {
-    //      templateUrl: '../../views/templates/nav.html',
-    //      controller: 'navController'
-    //    },
-    //    //'content': {
-    //    //  template:'<div ui-view></div>'
-    //    //},
-    //    },
-    //      templateUrl: '../../views/templates/discoverMusic.html',
-    //      controller: 'discoverMusicController'
-    //    },
-    //    'footer': {
-    //      templateUrl:'../../views/templates/footer.html'
-    //    }
-    //  }
-    //})
 
-    //.state('savedPlaylists', {
-    //  url: '/savedPlaylists',
-    //  templateUrl: '/views/templates/savedPlaylists.html',
-    //  controller: 'savedPlaylistsController'
-    //})
-    //.state('signIn', {
-    //  url: '/signIn',
-    //  templateUrl: '/views/templates/signIn.html',
-    //  controller: 'signInController'
-    //});
 });
