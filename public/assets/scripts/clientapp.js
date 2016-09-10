bf0f2d9182fdadac40b72aa5b48b4d8a2a04e225
Bennett Batzli <bennett.batzli@gmail.com>
2016-09-09 21:08:06 -0500

    configured ui routes in the html templates

--- public/assets/scripts/clientapp.js
+++ public/assets/scripts/clientapp.js
@@ -10,6 +10,19 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
       url: '/discoverMusic',
       templateUrl: '../../views/templates/discoverMusic.html',
       controller: 'discoverMusicController'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: '../../views/templates/about.html'
+    })
+    .state('signIn', {
+      url: '/signIn',
+      templateUrl: '../../views/templates/signIn.html',
+      controller: 'signInController'
+    })
+    .state('savedPlaylists', {
+      url: '/savedPlaylists',
+      templateUrl: '../../views/templates/savedPlaylists.html',
+      controller: 'savedPlaylistsController'
     });
-
 });
