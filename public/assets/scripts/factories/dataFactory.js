1a7cd84c2116d6f80e3b9be0260064fe437d245e
Bennett Batzli <bennett.batzli@gmail.com>
2016-09-16 21:01:41 -0500

    refactored http request to spotify api to occur in factory instead of controller

--- public/assets/scripts/factories/dataFactory.js
+++ public/assets/scripts/factories/dataFactory.js
@@ -1,10 +1,12 @@
-myApp.factory('DataFactory', ['$http', '$window', function($http, $window) {
+myApp.factory('DataFactory', ['$http', '$window', '$sce', function($http, $window, $sce) {
 
   //private
   var playlistNames = undefined;
   var playlistInfo = undefined;
 
   var playlistID = undefined;
+
+  var uriLink = undefined;
 
 
   // This happens after page load, which means it has authenticated if it was ever going to
@@ -77,10 +79,65 @@ myApp.factory('DataFactory', ['$http', '$window', function($http, $window) {
     return promise;
   };
 
+  var randomNumber = function (min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+  };
+
+  var privateDiscoverSong = function(selectedGenre) {
+    console.log('datafactory genre', selectedGenre);
+    //$scope.data = {};
+
+    // var request = baseURL + encodeURI(query) + '&callback=JSON_CALLBACK';
+    // console.log('the rquest!!!', request);
+    // var songsArray = ['*a*', '*o*', '*u*', '*i*', '*e*'];
+
+    // var songsArray = ['*a*', 'a*', '*e*', 'e*', '*i*', 'i*', '*o*', 'o*', '*y*'];
+
+    var songsArray = ['a'];
+
+    // var songsArray = ['%25a%25', 'a%25', '%25e%25', 'e%25', '%25i%25', 'i%25', '%25o%25', 'o%25'];
+    var randomTrack = songsArray[Math.floor(Math.random()*songsArray.length)];
+    var randomOffset = randomNumber(1, 100);
+    // var genre =
+    console.log('the wild card', randomTrack);
+
+    if(selectedGenre) {
+      var baseURL = "https://api.spotify.com/v1/search";
+      var query = "?q=";
+      query += "%20genre:%22" + selectedGenre + "%22";
+
+      query += "&offset=" + randomOffset;
+      query += "&limit=1&type=track&callback=JSON_CALLBACK";
+      var request = baseURL + query;
+
+      //var coolRequest = "https://api.spotify.com/v1/search?q=randomTrack&offset=randomOffset&limit=1&type=song&callback=JSON_CALLBACK";
+
+      var promise = $http.get(request).then(
+        function(response) {
+          console.log('response dataaaaa::', response.data);
+
+          console.log('the track name:', response.data.tracks.items[0].name);
+          console.log('the artist name:', response.data.tracks.items[0].artists[0].name);
+
+          console.log('the album name:', response.data.tracks.items[0].album.name);
+
+          return $sce.trustAsResourceUrl('https://embed.spotify.com/?uri=' + response.data.tracks.items[0].uri);
+        }
+      );
+      return promise;
+
+    } else {
+      alert("Please select a genre.");
+    }
+  };
+
   //public
   var publicApi = {
     factoryUserAuthenication: function() {
       return privateUserAuthentication();
+    },
+    discoverSong: function(selectedGenre) {
+      return privateDiscoverSong(selectedGenre);
     },
     factorySaveFavorite: function(favorite) {
       return privateSaveFavorite(favorite);
